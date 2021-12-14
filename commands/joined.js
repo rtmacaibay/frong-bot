6dd06b1c478821f489994bab44298175e92a6c6f
Robert Macaibay <rtmacaibay@usfca.edu>
2021-12-14 00:11:34 -0800

    Adjusted joined to include mentions

--- commands/joined.js
+++ commands/joined.js
@@ -2,8 +2,18 @@ module.exports = {
 	name: 'joined',
 	description: 'Displays when you joined the server',
 	aliases: ['j'],
-	args: false,
-	execute(message) {
-		return message.delete().then(message.channel.send(`${message.author} joined: ${message.member.joinedAt.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' })} PST`));
+	args: true,
+	usage: '<optional: @ a user to check their joined date',
+	execute(message, args) {
+		if (!args.length) {
+			const author = message.author;
+			const joinedDate = message.member.joinedAt.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+			return message.delete().then(message.channel.send(`${author} joined: ${joinedDate} PST`));
+		}
+		else {
+			const member = message.mentions.users.first();
+			const joinedDate = message.mentions.users.first().joinedAt.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+			return message.delete().then(message.channel.send(`${member} joined: ${joinedDate} PST`));
+		}
 	},
 };
