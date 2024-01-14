a09423f96d36d73afb003f3da2b6ca3e2a2c5227
Robert Macaibay <rtmacaibay@usfca.edu>
2024-01-14 15:43:23 -0800

    remove hashtags from description

--- frong.js
+++ frong.js
@@ -254,7 +254,10 @@ async function IsCarousel(quickvids_url) {
 						resolve({ carouselArr, description });
 					} else {
 						let carouselArr = [];
-						let description = resp.substring(resp.indexOf("\"", resp.indexOf("description:\"", resp.indexOf("const data ="))), resp.indexOf("\",", resp.indexOf("description:\"", resp.indexOf("const data ="))) + 1);
+						let description = resp.substring(resp.indexOf("\"", resp.indexOf("description:\"", resp.indexOf("const data ="))) + 1, resp.indexOf("\",", resp.indexOf("description:\"", resp.indexOf("const data ="))));
+						if (description.includes("#")) {
+							description = description.substring(0, description.indexOf("#"));
+						}
 						resolve({ carouselArr, description });
 					}
 				}
