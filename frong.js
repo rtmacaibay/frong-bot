570d6697bef63b8be73c1e1c74fb50be71a9c990
Robert Macaibay <rtmacaibay@usfca.edu>
2024-02-04 01:56:17 -0800

    Add Tiktok Photo Handling to URLs

--- frong.js
+++ frong.js
@@ -129,7 +129,7 @@ function InterpretMessage(message, prefix) {
 }
 
 function ExtractURLs(message) {
-	let tiktok_urls = message.match(/(https:\/\/(www\.)?(vt|vm)\.tiktok\.com\/[A-Za-z0-9]+|https:\/\/(vx)?tiktok\.com\/@[\w.]+\/video\/[\d]+\/?|https:\/\/(vx)?tiktok\.com\/t\/[a-zA-Z0-9]+\/?)/);
+	let tiktok_urls = message.match(/(https:\/\/(www\.)?(vt|vm)\.tiktok\.com\/[A-Za-z0-9]+|https:\/\/(vx)?tiktok\.com\/@[\w.]+\/(video|photo)\/[\d]+\/?|https:\/\/(vx)?tiktok\.com\/t\/[a-zA-Z0-9]+\/?)/);
 	if (tiktok_urls == null) {
 		tiktok_urls = message.match(/https:\/\/(vx)?tiktok\.com\/@[\w.]?\/video\/[\d]+\/?/);
 		if (tiktok_urls != null) {
