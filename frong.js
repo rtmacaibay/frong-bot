0f4ee42ec647de1e51838437070c3a1e52df2eb7
rtmacaibay <robmacaibay@gmail.com>
2024-07-11 03:44:30 -0700

    use reactions to delete bot processing urls

--- frong.js
+++ frong.js
@@ -173,28 +173,119 @@ async function ProcessURLs(message, tiktok_urls, instagram_urls, twitter_urls, r
 				for (let i = 1; i < embedArr.length; i++) {
 					embeds.push(new Discord.EmbedBuilder().setURL(quickvids.url).setImage(embedArr[i]));
 				}
-				message.channel.send({ content: `<@${message.author.id}> | [${usernameOutput}](${quickvids.url})${descriptionOutput}`, embeds: embeds, allowedMentions: { parse: [] }});
+				message.channel.send({ content: `<@${message.author.id}> | [${usernameOutput}](${quickvids.url})${descriptionOutput}`, embeds: embeds, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+				const collectorFilter = (reaction) => {
+					return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+				}
+
+				message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+					.then(collected => {
+						const reaction = collected.first();
+
+						if (reaction.emoji.name === '🗑️') {
+							message.delete();
+						}
+					});
 			} else {
-				message.channel.send({ content: `<@${message.author.id}> | [${usernameOutput}](${quickvids.url})${descriptionOutput}`, allowedMentions: { parse: [] }});
+				message.channel.send({ content: `<@${message.author.id}> | [${usernameOutput}](${quickvids.url})${descriptionOutput}`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+				const collectorFilter = (reaction) => {
+					return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+				}
+
+				message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+					.then(collected => {
+						const reaction = collected.first();
+
+						if (reaction.emoji.name === '🗑️') {
+							message.delete();
+						}
+					});
 			}
 		});
 	} else if (instagram_urls != null) {
 		let url = instagram_urls[0];
 		if (url.includes("reel")) {
-			message.channel.send({ content: `<@${message.author.id}> | [d.ddinstagram](${url.replace("https://instagram.com/", "https://d.ddinstagram.com/")})`, allowedMentions: { parse: [] }});
+			message.channel.send({ content: `<@${message.author.id}> | [d.ddinstagram](${url.replace("https://instagram.com/", "https://d.ddinstagram.com/")})`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+			const collectorFilter = (reaction) => {
+				return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+			}
+
+			message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+				.then(collected => {
+					const reaction = collected.first();
+
+					if (reaction.emoji.name === '🗑️') {
+						message.delete();
+					}
+				});
 		} else {
-			message.channel.send({ content: `<@${message.author.id}> | [ddinstagram](${url.replace("https://instagram.com/", "https://ddinstagram.com/")})`, allowedMentions: { parse: [] }});
+			message.channel.send({ content: `<@${message.author.id}> | [ddinstagram](${url.replace("https://instagram.com/", "https://ddinstagram.com/")})`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+			const collectorFilter = (reaction) => {
+				return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+			}
+
+			message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+				.then(collected => {
+					const reaction = collected.first();
+
+					if (reaction.emoji.name === '🗑️') {
+						message.delete();
+					}
+				});
 		}
 	} else if (twitter_urls != null) {
 		let url = twitter_urls[0];
-		message.channel.send({ content: `<@${message.author.id}> | [vxtwitter](${url.replace("https://twitter.com/", "https://vxtwitter.com/")})`, allowedMentions: { parse: [] }});
+		message.channel.send({ content: `<@${message.author.id}> | [vxtwitter](${url.replace("https://twitter.com/", "https://vxtwitter.com/")})`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+		const collectorFilter = (reaction) => {
+			return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+		}
+
+		message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+			.then(collected => {
+				const reaction = collected.first();
+
+				if (reaction.emoji.name === '🗑️') {
+					message.delete();
+				}
+			});
 	} else if (reddit_urls != null) {
 		let url = reddit_urls[0];
 		let { streamable_url, description } = await ProcessRedditURL(url);
 		if (url.match(streamable_url)) {
-			message.channel.send({ content: `<@${message.author.id}> | [rxddit](${url.replace("reddit.com/", "rxddit.com/").replace("redd.it/", "rxddit.com/")}) | ${description}`, allowedMentions: { parse: [] }});
+			message.channel.send({ content: `<@${message.author.id}> | [rxddit](${url.replace("reddit.com/", "rxddit.com/").replace("redd.it/", "rxddit.com/")}) | ${description}`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+			const collectorFilter = (reaction) => {
+				return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+			}
+
+			message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+				.then(collected => {
+					const reaction = collected.first();
+
+					if (reaction.emoji.name === '🗑️') {
+						message.delete();
+					}
+				});
 		} else {
-			message.channel.send({ content: `<@${message.author.id}> | [streamable](${streamable_url})`, allowedMentions: { parse: [] }})
+			message.channel.send({ content: `<@${message.author.id}> | [streamable](${streamable_url})`, allowedMentions: { parse: [] }}).then(() => message.react('🗑️'));
+
+			const collectorFilter = (reaction) => {
+				return ['🗑️'].includes(reaction.emoji.name) && !interaction.user.bot
+			}
+
+			message.awaitReactions({ filter: collectorFilter, max: 1, time: 900_000, errors: ['time']})
+				.then(collected => {
+					const reaction = collected.first();
+
+					if (reaction.emoji.name === '🗑️') {
+						message.delete();
+					}
+				});
 		}
 	}
 	if (seen) { message.delete(); }
