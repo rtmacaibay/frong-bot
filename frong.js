fe30094ddf50808a5b241a89d4a6f6a8b3ab178f
rtmacaibay <robmacaibay@gmail.com>
2024-07-12 02:29:05 -0700

    use interaction update

--- frong.js
+++ frong.js
@@ -233,6 +233,10 @@ function processURLReaction(message, original_url, author, delete_button, link_b
 		} else if (selection === 'link' && posted < 1) {
 			posted += 1;
 			message.channel.send({content: `${original_url}`, flags: [Discord.MessageFlags.SuppressEmbeds]});
+			interaction.update({
+				content: 'Link posted',
+				components: [],
+			});
 		}
 	});
 
